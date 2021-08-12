5c41e1a08ca89ef39a2521e51885eb1543e36f29
Pavel Savelev <pavel54.00@mail.ru>
2021-08-12 20:05:29 +1000

    com

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
         'use strict';
-  let money,
-      income, 
-      addExpenses, 
-      deposit, 
-      mission, 
-      period;
+  let money = 55000,
+      income = 'Такси',
+      addExpenses = 'Интернет, Квартира, Бензин',
+      deposit = true, 
+      mission = 100000, 
+      period = 5,
+      budgetDay = money / 30;
 
-      alert('Привет Друг!');
-
+      //alert('Привет Друг!');
       console.log('Сообщение в консоли' );
+
+      console.log(typeof money);
+      console.log(typeof income);
+      console.log(typeof deposit);
+
+      console.log(addExpenses.length);
+
+      console.log('Период равен ' + period + ' месяцев');
+
+      console.log('Цель заработать ' + mission + ' рублей');
+
+       console.log(addExpenses.toLowerCase());
+      console.log(addExpenses.split());
+
+      console.log(budgetDay);
+
+
+      
