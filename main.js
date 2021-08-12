638dad0d83193e9a575c718563520754759cfe7e
Pavel Savelev <pavel54.00@mail.ru>
2021-08-12 19:24:05 +1000

    add new branch lesson01

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+        'use strict';
+  let money,
+      income, 
+      addExpenses, 
+      deposit, 
+      mission, 
+      period;
 
+      alert('Привет Друг!');
+
+      console.log('Сообщение в консоли' );
